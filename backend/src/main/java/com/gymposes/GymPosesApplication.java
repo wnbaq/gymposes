d973f8517974ba3f2b4b800d1fac57e03cc68563
begüm bingöl <goksubegumbingol@gmail.com>
2026-06-24 23:03:22 +0300

    chore: initialize Spring Boot project with dependencies

---
+++ backend/src/main/java/com/gymposes/GymPosesApplication.java
@@ -0,0 +1,11 @@
+package com.gymposes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymPosesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GymPosesApplication.class, args);
+    }
+}
